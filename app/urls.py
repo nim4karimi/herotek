e8539fa8f3c192b23134cfdbbf8b41dbe9976671
Nima <nim4.karimi@gmail.com>
2018-03-02 00:37:44 +0330

    DisableSSLChangUrl

--- app/urls.py
+++ app/urls.py
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path , include
-from calendarapp import views
-
+from django.urls import path
+from .views import index , service
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('calendar/', views.calendar),
-    path('', include('app.urls'))
+    path('', index),
+    path('service/', service , name="service"),
 ]
