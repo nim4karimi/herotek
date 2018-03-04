09f08224c31ad0194677263803e219c3dc23765f
Nima karimi <nima.karimi@live.com>
2018-03-04 12:46:50 +0330

    FixCaptchaa

--- herotek/urls.py
+++ herotek/urls.py
@@ -1,3 +1,22 @@
+
+#from django.contrib import admin
+from django.urls import path , include
+from calendarapp import views
+
+from captcha_admin import admin
+# This should stay the same
+admin.autodiscover()
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('calendar/', views.calendar),
+    path('', include('app.urls'))
+]
+
+
+
+
+
 """herotek URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,15 +32,3 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path , include
-from calendarapp import views
-from captcha_admin import admin
-# This should stay the same
-admin.autodiscover()
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('calendar/', views.calendar),
-    path('', include('app.urls'))
-]
