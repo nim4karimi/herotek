3b3f7e3dd3755a73a2ef5f3efd705a8c0b9017c6
Nima karimi <nima.karimi@live.com>
2018-03-11 13:17:24 +0330

    Add Live Chat

--- herotek/settings.py
+++ herotek/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'kh7(@-j$2-smn10(hcm&*&qtrg*=og8@#iz&xv5+xv$marjrqv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['.herotek.ir','127.0.0.1']
+ALLOWED_HOSTS = ['.herotek.ir','127.0.0.2']
 
 
 # Application definition
