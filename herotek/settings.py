75c29683517b2ceb02849b46c1d0b7ba696f47c8
Nima <nim4.karimi@gmail.com>
2018-03-13 23:02:34 +0330

    Optimize title + footer links

--- herotek/settings.py
+++ herotek/settings.py
@@ -128,11 +128,9 @@ NOCAPTCHA = True
 #LOGIN 
 LOGIN_REDIRECT_URL = "/"
 
-# Some Setting for config SSL
+# Some Setting for config 
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE= True
-
-
