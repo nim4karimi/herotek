872b2af851d1ed982675b39cfd4a64e988b44f40
Nima Karimi <nima.karimi@live.com>
2018-05-13 16:08:16 +0430

    FIX - Blog Body text

--- blog/views.py
+++ blog/views.py
@@ -13,10 +13,11 @@ class EntryDetailView(DetailView):
         context['now'] = timezone.now()
         return context
 
+
+
 class EntryListView(ListView):
 
     model = Entry
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['now'] = timezone.now()
