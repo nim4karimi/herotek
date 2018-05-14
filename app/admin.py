bebff7dbdd89543f1b408439ec74e96c989a31cb
Nima Karimi <nima.karimi@live.com>
2018-05-14 13:03:40 +0430

    Fix - Summernote field in service & prod

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,19 @@
 from captcha_admin import admin
+from django_summernote.admin import SummernoteModelAdmin
 
 from .models import Service , Prod
 
-admin.site.register(Service)
-admin.site.register(Prod)
+class ServicePost(SummernoteModelAdmin):
+    summernote_fields = ('desc',)
+
+
+admin.site.register(Service , ServicePost)
+
+
+
+class ProdPost(SummernoteModelAdmin):
+    summernote_fields = ('desc','body')
+
+
+admin.site.register(Prod, ProdPost)
+
